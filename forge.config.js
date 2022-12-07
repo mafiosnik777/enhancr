50fb0789911d0a092c181ac029631f3a3537cc4e
IcedShake <81792933+IcedShake@users.noreply.github.com>
2022-12-08 01:22:57 +0800

    Improve build for css & moving forge stuff around

--- forge.config.js
+++ forge.config.js
@@ -1,9 +1,41 @@
+const fs = require('fs-extra');
+const path = require('path');
+const sass = require('sass');
+
+const assetBuildDir = path.resolve(__dirname, './build');
+const includePaths = [
+    'src',
+    'build',
+    'LICENSE',
+    'package.json',
+    'node_modules',
+];
+
+const ignoredPaths = fs.readdirSync(__dirname).filter((pathname) => (
+    !includePaths.includes(pathname)
+));
+
 if (process.platform === 'win32') process.env.GYP_MSVS_VERSION = '2017';
 
 module.exports = {
     packagerConfig: {
         icon: './src/assets/enhancr-icon',
         asar: false,
+        ignore: [
+            'src/scss',
+            ...ignoredPaths,
+        ],
+    },
+    hooks: {
+        generateAssets: async () => {
+            await fs.ensureDir(assetBuildDir);
+            await fs.emptyDir(assetBuildDir);
+
+            await fs.writeFile(
+                path.resolve(assetBuildDir, 'app.min.css'),
+                sass.compile('./src/scss/app.scss', { style: 'compressed' }).css,
+            );
+        },
     },
     makers: [
         {
