e3af55eb9d87864a0497590793c64ac2fe3a4100
Erwin Bosch <mafiosnik777@protonmail.com>
2022-12-22 13:33:23 +0100

    render correct version in log

--- src/js/log.js
+++ src/js/log.js
@@ -1,3 +1,4 @@
+const { app } = require('@electron/remote');
 var fse = require('fs-extra');
 var path = require('path');
 const os = require('os');
@@ -12,7 +13,7 @@ window.addEventListener("error", (event) => {
     fse.appendFileSync(path.join(appDataPath, '/.enhancr/', 'log.txt'), '\n\n [renderer process error] \n\n' + stringifyObject(event) + '\n\n');
 });
 
-fse.writeFile(path.join(appDataPath, '/.enhancr/', 'log.txt'), 'enhancr build 0.9.3a (pre-release)\n\n' + terminal.innerHTML);
+fse.writeFile(path.join(appDataPath, '/.enhancr/', 'log.txt'), `enhancr build ${app.getVersion()} (pre-release)\n\n` + terminal.innerHTML);
 fse.writeFile(os.tmpdir() + '/tmpLog.txt', terminal.innerHTML);
 
 async function log() {
