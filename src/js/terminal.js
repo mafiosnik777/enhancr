3e7023d2dbf5401e0a0fd7ebb045f03bd52b7cab
mafiosnik <108760201+mafiosnik777@users.noreply.github.com>
2023-03-27 17:19:26 +0200

    prevent dragging of terminal text, locking up the app

--- src/js/terminal.js
+++ src/js/terminal.js
@@ -6,3 +6,8 @@ terminal.textContent += "\r\n" + localStorage.getItem('cpu');
 terminal.textContent += "\r\n" + localStorage.getItem('ram');
 terminal.textContent += "\r\n" + localStorage.getItem('gpu');
 terminal.textContent += "\r\nStatus: Ready\r\n•°•────────────────────────────────•°•";
+
+// prevent dragging of terminal content
+terminal.addEventListener('dragstart', function(event) {
+    event.preventDefault();
+});
